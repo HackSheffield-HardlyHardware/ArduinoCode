041e243f84462d1a67f25cf0c9333dde2639ee8d
Samuel Gibbs <Samuel@gibbsfamily.org.uk>
2018-10-27 20:14:39 +0100

    Update HS2/HS2.ino
    LED for arrow direction

--- HS2/HS2.ino
+++ HS2/HS2.ino
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 
+#include <Adafruit_NeoPixel.h>
+#define PIN 9  //// what pin are the NeoPixels connected to?
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, PIN, NEO_GRB + NEO_KHZ800);  /// the strip is 30 pixels long.  You can change this for the number of pixels in your individual strip.
+
+
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
@@ -21,45 +26,56 @@ AxisJoystick* joystick;
 bool alarm = false;
 int i = 0;
 void setup() {
-  
   // put your setup code here, to run once:
   joystick = new AxisJoystick(SW_PIN, VRX_PIN, VRY_PIN);
   u8g2.begin();
-<<<<<<< HEAD
-
-=======
-  Wire.setClock(40000L);
-  drawOff();
->>>>>>> a16b8ebc39ae7ec15536b0954ce8258da4a515e8
   u8g2.sendBuffer();
+  
+  strip.begin();
+  strip.show(); //Initialise all pixels off
 }
 
 void drawUp()
 {
   u8g2.setFont(u8g2_font_open_iconic_all_8x_t);
   u8g2.drawGlyph(30, 80, 0x0077);
+  SetLEDS(255,0,0);
 }
 
 void drawDown()
 {
   u8g2.setFont(u8g2_font_open_iconic_all_8x_t);
   u8g2.drawGlyph(30, 80, 0x0074);
+  SetLEDS(0,255,0);
+
 }
 void drawLeft()
 {
   u8g2.setFont(u8g2_font_open_iconic_all_8x_t);
   u8g2.drawGlyph(30, 80, 0x0075);
+  SetLEDS(0,0,255);
 }
 void drawRight()
 {
   u8g2.setFont(u8g2_font_open_iconic_all_8x_t);
   u8g2.drawGlyph(30, 80, 0x0076);
+  SetLEDS(128,128,128);
 }
 
+void SetLEDS(int red, int green , int blue)
+{
+    for (int i = 0; i <30;i++)
+  {
+    strip.setPixelColor(i, red, green, blue);  
+  }
+  strip.show();
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
   const AxisJoystick::Move move = joystick->multipleRead();
+
+
   if(move == AxisJoystick::Move::PRESS) {
     alarm = !alarm;
     i=0;
